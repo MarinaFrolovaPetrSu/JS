714d8de488ed857573309a13a4b6a3e2e678027a
MarinaFrolovaPetrSu <47503570+MarinaFrolovaPetrSu@users.noreply.github.com>
2020-11-30 05:29:58 +0300

    hw 6

---
+++ projects/geo-review/js/api.js
@@ -0,0 +1,60 @@
+const database = {};
+
+const delay = 500;
+
+function _toString(coords) {
+  if (Array.isArray(coords)) {
+    coords = coords.join(',');
+  }
+
+  return coords;
+}
+
+function getPlacmarks() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(JSON.parse(localStorage.getItem('placemarks')));
+    }, delay);
+  });
+}
+
+async function getPlacmark(coords) {
+  coords = _toString(coords);
+
+  const placemarks = JSON.parse(localStorage.getItem('placemarks'));
+
+  if (placemarks) {
+    var placemark = placemarks[coords] ? placemarks[coords] : null;
+  } else {
+    var placemark = null;
+  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(placemark);
+    }, delay);
+  });
+}
+
+async function setPlacmark(coords, payload) {
+  coords = _toString(coords);
+
+  const placemarks = JSON.parse(localStorage.getItem('placemarks')) || {};
+  let status = false;
+
+  if (placemarks[coords]) {
+    placemarks[coords].push(payload);
+  } else {
+    placemarks[coords] = [payload];
+    status = true;
+  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      localStorage.setItem('placemarks', JSON.stringify(placemarks));
+      resolve(status ? { [coords]: placemarks[coords] } : null);
+    }, delay);
+  });
+}
+
+export { getPlacmarks, getPlacmark, setPlacmark };
